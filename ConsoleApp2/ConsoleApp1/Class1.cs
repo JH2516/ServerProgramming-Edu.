db77952b2eb1139077f35b408e6e2e8532fdb804
unknown <kaizer2516@gmail.com>
2024-04-18 11:47:59 +0900

    week 7 updated

---
+++ ConsoleApp2/ConsoleApp1/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Diagnostics;
+
+namespace ConsoleApp2
+{
+
+    internal class Class1
+    {
+        private static Socket sock;
+        static void main(string[] args)
+        {
+            var localEPIP = new IPEndPoint(IPAddress.Parse("172.18.27.34"), 25000);
+            var localEPLoopback = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 25000);
+            localEPLoopback = new IPEndPoint(IPAddress.Loopback, 25000);
+            var localEPAll = new IPEndPoint(IPAddress.Parse("0.0.0.0"), 25000);
+            localEPAll = new IPEndPoint(IPAddress.Any, 25000);
+
+            StartServer(localEPAll);
+        }
+        private static void StartServer(IPEndPoint localEPAll)
+        {
+            try
+            {
+                sock = new Socket(AddressFamily.InterNetwork,
+                    SocketType.Stream, ProtocolType.Tcp);
+                //EndPoint is abstract class
+                
+                sock.Bind(localEPAll);
+                Console.WriteLine($"[info] -- Server Bind to [{sock.LocalEndPoint}]");
+                Console.WriteLine($"[info] -- Server Bind to [{localEPAll.Address}]:[{localEPAll.Port}]");
+
+                Console.WriteLine("[info] -- Server Listenning with limit 10");
+                sock.Listen(10);
+                Console.WriteLine("[info] -- Server Listen started");
+
+                //접속자가 나타날때까지 동기식 대기상태로 대기
+                Console.WriteLine("[info] -- Server blocking in Accept()");
+                var clientSock = sock.Accept();
+                Console.WriteLine("[info] -- Client connected");
+                IPEndPoint clientEp = (IPEndPoint)clientSock.RemoteEndPoint;
+                Console.WriteLine($"[Client] -- from [{clientEp.Address}]:[{clientEp.Port}]");
+
+                //데이터 송수신
+                Console.WriteLine("[info] -- Transfer data from client to server");
+                //Thread.Sleep(1000);
+                Console.ReadLine();
+                //접속 종료(아무나)
+                clientSock.Close();
+                Console.WriteLine("[info] -- Client Closed by Server");
+            }
+            catch (Exception ex)
+            { 
+                Console.WriteLine(ex.ToString());
+            }
+            sock.Close();
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Diagnostics;
+
+namespace console_tcp_00
+{
+    internal class Program
+    {
+        //private socket = Socket.socket;
+        static void Main(string[] args)
+        {
+            var hostEntry = Dns.GetHostEntry("www.google.com");
+            var remoteEP = new IPEndPoint(hostEntry.AddressList[0], 80);
+            sendTcpWsg(remoteEP, "GET/HTTP/1.1\r\n\r\n");
+            var remoteIP = new IPEndPoint(IPAddress.Parse("172.18.27.34"), 25000);
+            sendTcpWsg(remoteIP, "helloFirst app\r\n");
+        }
+        private static void sendTcpWsg(IPEndPoint remoteEP, string v)
+        {
+            try
+            {
+                Socket socket = new Socket(AddressFamily.InterNetwork,
+                    SocketType.Stream, ProtocolType.Tcp);
+                socket.Connect(remoteEP);
+                Console.WriteLine($"[info] -- connection to Server" + 
+                    $"{remoteEP.Address}{remoteEP.Port}");
+                byte[] buffer = Encoding.UTF8.GetBytes(v);
+                int size = buffer.Length;
+
+                //데이터 송신
+                Console.WriteLine(v);
+                int retval = socket.Send(buffer, 0,size,SocketFlags.None);
+                Console.WriteLine("[info] -- send finish -- !");
+
+
+                //데이터 수신
+                buffer = new byte[1500];
+                retval = socket.Receive(buffer,0,buffer.Length, SocketFlags.None);
+                Console.WriteLine($"[info] -- recvbytes[{retval}]");
+                Console.WriteLine($"[data] ==> [{buffer.ToString()}]");
+                for (int i = 0; i < buffer.Length; i++)
+                {
+                    if (buffer[i] == 0x0d || buffer[i] == 0x0a)
+                    {
+                        buffer[i] = 0x21;
+                    }
+                }
+                Console.WriteLine($"[data] ==> [{Encoding.UTF8.GetString(buffer)}]");
+                Console.WriteLine("The End");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+    }
+}
