c76d98fb953905bb6bb233b613416a566cb4b684
unknown <kaizer2516@gmail.com>
2024-05-02 11:35:56 +0900

    week9 uploaded

--- tcpClient_fixed_model.cs
+++ tcpClient_fixed_model.cs
@@ -6,7 +6,12 @@ namespace tcpClient_fixed_model
 {
     internal class Program
     {
-        static readonly int FIXED_BUFSIZE = 40;
+        //실제 시뮬하려면 소켓 수신 버퍼를 40 이하로 줄여놓고 실행
+        //발송하는 쪽에서 40 미만 1~2개로 100msec 인터벌
+        static readonly int FIXED_BUFSIZE = 100;
+        static readonly string terminalStr = "\n";
+        static readonly string terminalStr2 = "END";
+        static readonly byte[] terminalBufferStatic = Encoding.UTF8.GetBytes(terminalStr);
         static void Main(string[] args)
         {
             IPEndPoint remoteEP = new IPEndPoint(IPAddress.LoopBack, 25000);
@@ -20,15 +25,44 @@ namespace tcpClient_fixed_model
 
             string commandString = "GET Weather/Temp\r\n";
             byte[] temp = Encoding.UTF8.GetBytes(commandString);
-            byte[] buffer = new byte[40];
-            //생성후 문자열 넣기
+            byte[] buffer = new byte[FIXED_BUFSIZE];
+            byte[] terminalBuff = Encoding.UTF8.GetBytes(terminalStr);
+            byte[] size = new byte[sizeof(int)];
+            size = BitConverter.GetBytes(temp.Length);
+            // 호스트 2 네트워크 바이트 오버 바이트 순서 변경
+            //합배송
+            byte[] fullBytes = new byte[temp.Length + size.Length];
+            Array.copy(size, fullBytes, size.Length);
+            Array.Copy(temp,0,fullBytes,size.Length,temp.Lengths)
+            //문자열 녛기
             Array.Copy(temp, buffer, temp.Length);
-            for (int cnt = temp.Length; cnt < buffer.Length; cnt++)
+            Array.Copy(terminalBuff, 0, buffer, temp.Length,terminalBuff);
+            //생성후 문자열 넣기
+            //배열을 삽입하거나 빼기 위한 과정
+            //소스, 도착지, 길이
+            Array.Copy(temp, buffer, temp.Length);
+            //Array.Copy(terminalStr.ToCharArray(), 0, buffer, temp.Length + terminalStr.Length, terminalStr.Length);//이건 말도 안된다고!
+            Array.Copy(terminalBuff, 0, buffer, temp.Length+terminalStr.Length, terminalStr.Length);
+            int buffSize = temp.Length+ terminalStr.Length + terminalBuff.Length;
+            for (int cnt = buff.Length; cnt < buffer.Length; cnt++)
             {
                 buffer[cnt] = (Byte)'#';
             }
             sock.Connect(remoteEP);
-            sock.Send(buffer);
+            //보내기
+            //분리 전송
+            sock.Send(size);
+            sock.Send(temp);
+            //합전송
+            sock.Send(fullbytes);
+            //sock.Send(buffer);
+            //받기
+            int retval = sock.Receive(buffer,0,FIXED_BUFSIZE, SocketFlags.None);
+            while(retval < 40)
+            {
+                retval += sock.Receive(buffer, 0, FIXED_BUFSIZE, SocketFlags.None);
+                //메세지를 의미있는 단위로 재합성해서 큐 같은 것들에 저장한다
+            }
             sock.Close();
         }
     }
---
+++ tcpServer_fixed_model.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace tcpClient_fixed_model
+{
+    internal class Program
+    {
+        //실제 시뮬하려면 소켓 수신 버퍼를 40 이하로 줄여놓고 실행
+        //발송하는 쪽에서 40 미만 1~2개로 100msec 인터벌
+        //프로토콜 사이즈가 동일하다
+        static readonly int FIXED_BUFSIZE = 100;
+        static void Main(string[] args)
+        {
+            IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 25000);
+            StartServerWithFixedType(localEP);
+        }
+        private static void StartServerWithFixedType(IPEndPoint localEP)
+        {
+            //소켓
+            Socket listensock = new Socket(AddressFamily.InterNetwork, SocketType,Stream, Program.FIXED_BUFSIZE);
+            //바인드,listen
+            listensock.Bind(localEP);
+            listensock.Listen(10);
+            //accept
+            while (listensock.Connected)
+            {
+                //accept
+                Socket clientSock = listensock.Accept();
+                Console.WriteLine("[info] -- waiting Connection with New client");
+                IPEndPoint clientEP = (IPEndPoint)clientSock.RemoteEndPoint;
+                Console.WriteLine($"[info] -- connection With [{clientEP.Address}]:[{clientEP.ToString()}]");
+                byte[] buffer = new byte [1500];
+
+                //send,recv
+                Console.WriteLine("[info] -- RECV waiting");
+                byte[] size = new byte[sizeof(int)];
+                int retSizeval = clientSock.Receive(size,0,size.Length,SocketFlags.None);
+                Console.Writeline($"[RECV] --> recvBytes : retSizeValue [{retsizeval}]");
+                int realDataSize = BitConverter.ToInt32(size);
+                byte[] buffer = new byte[realDataSize];
+                int retval = clientEP.Receive(buffer, 0,buffer.Length,SocketFlags.None);
+                Console.WriteLine($"[RECV] --> recvBytes[{retval}]");
+                Console.WriteLine($"[RECV Raw Data] --> [{Encoding.UTF8.GetString(buffer)}]");
+
+                //send,rev 2
+                byte[] size = new byte[sizeof(int)];
+                int retSizeval = clientSock.Receive(size, 0, size.Length, SocketFlags.None);
+                Console.Writeline($"[RECV2] --> recvBytes : retSizeValue [{retsizeval}]");
+                int realDataSize = BitConverter.ToInt32(size);
+                byte[] buffer = new byte[realDataSize];
+                int retval = clientEP.Receive(buffer, 0, buffer.Length, SocketFlags.None);
+                Console.WriteLine($"[RECV2] --> recvBytes[{retval}]");
+                Console.WriteLine($"[RECV2 Raw Data] --> [{Encoding.UTF8.GetString(buffer)}]");
+                //구문 분석 및 처리
+                //찾기
+                string recvRawData = Encoding.UTF8.GetString(buffer);
+                if(recvRawData.Contains(terminalStr);)
+                {
+                    int idx = recvRawData.IndexOf(terminalStr);
+                    //if(recvRawData.Length == idx)
+                    //{
+                        byte[] data = new byte[recvRawData.Length - idx];
+                        Array.Copy(buffer, 0, data, 0, idx-1);
+                        Console.WriteLine($"[RECV Data] --> [{Encoding.UTF8.GetString(data)}]");
+                    //}
+                }
+                //응답 전송 및 처리
+                //연결 끊음
+                Console.WriteLine("[info] -- client closed");
+                clientSock.Close();
+            }
+            //서버 죽음
+            listensock.Close();
+        }
+    }
+}
